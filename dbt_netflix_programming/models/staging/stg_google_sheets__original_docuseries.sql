96d9d9f2ed76f8c70f89dfefd1a6fdb9e38ef707
dextersworkshop <106976383+dextersworkshop@users.noreply.github.com>
2022-12-07 14:50:40 -0800

    standardized source data in staging model(s)

--- dbt_netflix_programming/models/staging/stg_google_sheets__original_docuseries.sql
+++ dbt_netflix_programming/models/staging/stg_google_sheets__original_docuseries.sql
@@ -7,7 +7,16 @@ with source as (
 
 standardized as (
 
-    select *
+    select 
+        -- keep only title which exists between astericks (not perfect solution BUT good enough for now)
+        regexp_substr(title, '([^*]+)') as title
+      , genre
+      , regexp_replace(premiere, '\\[[[:alnum:]]+\\]') as premiere_date
+      , split_part(premiere, ' ', 1) as premiere_month
+      , seasons
+      , runtime
+      , regexp_replace(status, '\\[[[:alnum:]]+\\]') as status
+      , updated_at
     from source
 
 )
